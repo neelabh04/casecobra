8ccfc3e57e67d92632d2c2fceed3466262f43cf2
Neelabh Singh <neelabh041997@gmail.com>
2024-05-10 11:50:16 +0530

    first commit

---
+++ src/config/products.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_PRICES = {
+  material: {
+    silicone: 0,
+    polycarbonate: 200_00,
+  },
+  finish: {
+    smooth: 0,
+    textured: 250_00,
+  },
+} as const
+
+export const BASE_PRICE = 140_00
